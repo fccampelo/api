290fc87596002a4483d48bf0320dd0c2044ef76a
Felipe Campelo <fcampelocc@gmail.com>
2019-07-04 01:19:19 -0300

    feat(Database): creating connection to the database

--- src/app.js
+++ src/app.js
@@ -1,14 +1,29 @@
 import express from "express";
+import mongoose from "mongoose";
 
 class App {
   constructor() {
     this.server = express();
 
     this.middlewares();
+    this.connectDateBase();
   }
 
   middlewares() {
     this.server.use(express.json());
+  }
+
+  connectDateBase() {
+    try {
+      mongoose.connect(
+        "mongodb://user:user123@ds117489.mlab.com:17489/connector",
+        {
+          useNewUrlParser: true,
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
